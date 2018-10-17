e6161126b6a8da3afce3be47ff3b8aedfbe22b3d
Yuval Zehavi <YUVALZE@amdocs.com>
2018-10-17 23:30:55 +0300

    add files

---
+++ src/actions/shared.js
@@ -0,0 +1,16 @@
+import { getInitialData } from '../utils/api'
+import { receiveUsers } from '../actions/users'
+import { showLoading, hideLoading } from 'react-redux-loading'
+
+const AUTHED_ID = 'tylermcginnis'
+
+export function handleInitialData () {
+  return (dispatch) => {
+    dispatch(showLoading())
+    return getInitialData()
+      .then(({ users, tweets }) => {
+        dispatch(receiveUsers(users))
+        dispatch(hideLoading())
+      })
+  }
+}
