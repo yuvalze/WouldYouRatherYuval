082db72a13922b1791016b3ae674ed8ab67c6a86
Yuval Zehavi <YUVALZE@amdocs.com>
2018-10-19 17:49:12 +0300

    fix start errors

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -2,13 +2,11 @@ import { getInitialData } from '../utils/api'
 import { receiveUsers } from '../actions/users'
 import { showLoading, hideLoading } from 'react-redux-loading'
 
-const AUTHED_ID = 'tylermcginnis'
-
 export function handleInitialData () {
   return (dispatch) => {
     dispatch(showLoading())
     return getInitialData()
-      .then(({ users, tweets }) => {
+      .then (users => {
         dispatch(receiveUsers(users))
         dispatch(hideLoading())
       })
--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,7 @@ import React, { Component, Fragment } from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { handleInitialData } from '../actions/shared'
-import Dashboard from './Dashboard'
 import LoadingBar from 'react-redux-loading'
-import NewTweet from './NewTweet'
-import TweetPage from './TweetPage'
 import Nav from './Nav'
 
 class App extends Component {
@@ -22,9 +19,7 @@ class App extends Component {
             {this.props.loading === true
               ? null
               : <div>
-                  <Route path='/' exact component={Dashboard} />
-                  <Route path='/tweet/:id' component={TweetPage} />
-                  <Route path='/new' component={NewTweet} />
+                bla
                 </div>}
           </div>
         </Fragment>
