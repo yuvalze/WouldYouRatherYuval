a9448db860c67861c665da7dc7a866f6662afb05
Yuval Zehavi <YUVALZE@amdocs.com>
2018-10-24 20:46:03 +0300

    remove authUser when logout

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,10 @@
 import React, { Component, Fragment } from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { handleInitialData } from '../actions/shared'
 import LoadingBar from 'react-redux-loading'
 import SignIn from './SignIn'
+import Logout from './Logout'
 import HomePage from './HomePage'
 import { setAuthedUser } from '../actions/authedUser'
 
@@ -17,15 +18,20 @@ class App extends Component {
     this.props.dispatch(setAuthedUser(userId))
   }
 
+  onUserLogOut() {
+    this.props.dispatch(setAuthedUser(undefined));
+  }
+
   render() {
+    console.log('App render');
     return (
       <Router>
         <Fragment>
           <LoadingBar />
           <div className='container'>
             {this.props.loading === true
-              ? null
-              : <div>
+              ? null :
+                <div>
                   <Route path='/' exact render={({ history }) => (
                     <SignIn
                       users={this.props.users[0]}
@@ -34,9 +40,26 @@ class App extends Component {
                       history.push('/homePage')
                       }}
                     />
-                  )}/>  
-                  <Route path='/homePage' component={HomePage} />  
-                </div>}
+                  )}/>
+                  <Route path='/homePage' render={() => (
+                      !this.props.authedUser ? (
+                          <Redirect to='/'/>
+                      ) : (
+                          <HomePage/>
+                      )
+                  )}/>
+                  <Route path='/logout' render={({ history }) => (
+                    <Logout
+                        users={this.props.users[0]}
+                        onUserLogOut={()=> this.onUserLogOut}
+                        onUserLogIn={(userId) => {
+                            this.onUserLogIn(userId)
+                            history.push('/homePage')
+                        }}
+                    />
+                  )}/>
+                </div>
+            }
           </div>
         </Fragment>
       </Router>
@@ -44,10 +67,11 @@ class App extends Component {
   }
 }
 
-function mapStateToProps ({ users }) {
+function mapStateToProps ({ users, authedUser }) {
   return {
     loading: (users === null || users === undefined || users[0] === undefined),
-    users
+      users,
+      authedUser
   }
 }
 
---
+++ src/components/Logout.js
@@ -0,0 +1,33 @@
+import React, { Component} from 'react'
+import { connect } from 'react-redux'
+import 'react-dropdown/style.css'
+import PropTypes from 'prop-types';
+import SignIn from './SignIn'
+
+
+class Logout extends Component {
+
+    componentDidMount() {
+        console.log('Logout componentDidMount');
+        this.props.onUserLogOut();
+    }
+
+    render() {
+        return(
+            <SignIn
+                users={this.props.users}
+                onUserLogIn={this.props.onUserLogIn}
+            />
+        )
+    }
+}
+
+Logout.propTypes = {
+    users: PropTypes.object.isRequired,
+    onUserLogIn: PropTypes.func.isRequired,
+    onUserLogOut: PropTypes.func.isRequired
+};
+
+export default Logout
+
+
