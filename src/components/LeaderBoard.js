32a7708e788a1a120c114201d54d46510bacf60a
Yuval Zehavi <none>
2018-11-14 22:56:36 +0200

    Start LeaderBoard

--- src/components/LeaderBoard.js
+++ src/components/LeaderBoard.js
@@ -1,14 +1,31 @@
 import React, {Component} from 'react'
+import { connect } from 'react-redux'
 
 class LeaderBoard extends Component {
 
     render() {
+        const usersValueArr = Object.values(this.props.users); 
         return (
             <div>
                 <h1> The Leader Board !</h1>
+                <table>
+                    {usersValueArr.map(userObj =>
+                        <tr>
+                            {userObj.name}
+                        </tr>
+                        )
+                    }
+                </table>
             </div>
         )
     }
 }
 
-export default LeaderBoard
+
+function mapStateToProps ({ users}) {
+    return {
+        users
+    }
+  }
+  
+export default connect(mapStateToProps)(LeaderBoard)
